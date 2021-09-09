280c3d9fef75b049bec806194d82c5986ff3a9e2
asamaras <agorastos-dimitrios.samaras@nokia.com>
2021-09-09 15:30:25 +0300

    the skeleton of the server is ready

---
+++ go.mod
@@ -0,0 +1,9 @@
+module pcmd_server
+
+go 1.16
+
+require (
+	github.com/gin-gonic/gin v1.7.4 // indirect
+	github.com/google/martian v2.1.0+incompatible // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+)
+
+func storeData(ctx *gin.Context) {
+	// Validate input
+	var input map[interface{}]interface{} //TODO(Nikos K): you should bind to input the type that suits you best.
+	if err := ctx.ShouldBindJSON(&input); err != nil {
+		log.Error("Unable to handle request --> ", err)
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	log.Info("Storing/Modifying received Data")
+	// TODO(Nikos K): here you can modify to csv your json data and store
+	// them accordingly
+	ctx.JSON(http.StatusCreated, input) // or c.IndentedJSON(http.StatusNotFound, gin.H{"message": "data stored."})
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "This PCMD server receives data in JSON format.",
+		})
+	})
+	r.POST("/", storeData)
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080") the same as r.Run("localhost:8080")
+}
